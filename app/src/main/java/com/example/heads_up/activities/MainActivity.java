239f6770695a72fe669902b4fc3d2644393570a7
jfelix61 <rtfire456@hotmail.com>
2019-07-24 00:05:08 -0400

    Los categorías ya se reciben del servidor json

--- app/src/main/java/com/example/heads_up/activities/MainActivity.java
+++ app/src/main/java/com/example/heads_up/activities/MainActivity.java
@@ -2,15 +2,64 @@ package com.example.heads_up.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.heads_up.R;
+import com.example.heads_up.models.Category;
+import com.example.heads_up.services.CategoryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list;
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+        list = findViewById(R.id.category_list);
+
+        list.setAdapter(arrayAdapter);
+
+        getCategories();
+    }
+
+    private void getCategories() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        CategoryService categoryService = retrofit.create(CategoryService.class);
+        Call<List<Category>> call = categoryService.getCategories();
+
+        call.enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                for(Category category : response.body()) {
+                    titles.add(category.getCategoryName());
+                }
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+            }
+        });
     }
 }
