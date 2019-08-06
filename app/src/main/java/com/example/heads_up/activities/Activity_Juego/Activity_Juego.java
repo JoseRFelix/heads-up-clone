abe8aa4cb8fc1be4179cbb0f2a4321e6c3a117e0
adrianamota <lishamota@hotmail.com>
2019-08-06 02:10:02 -0400

    Se removió el botón de empezar, ahora empezará de forma automática

--- app/src/main/java/com/example/heads_up/activities/Activity_Juego/Activity_Juego.java
+++ app/src/main/java/com/example/heads_up/activities/Activity_Juego/Activity_Juego.java
@@ -19,7 +19,6 @@ import com.example.heads_up.activities.Activity_Main.CategoryListViewAdapter;
 
 public class Activity_Juego extends AppCompatActivity {
     private TextView countdownText;
-    private Button countdownButton;
 
     private CountDownTimer countDownTimer;
     private long timeLeftinMilliseconds = 60000; //1 min
@@ -59,14 +58,9 @@ public class Activity_Juego extends AppCompatActivity {
 
 
         countdownText = findViewById(R.id.timer);
-        countdownButton = findViewById(R.id.countdownButton);
 
-        countdownButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startStop();
-            }
-        });
+
+        startTimer();
 
         updateTimer();
     }
@@ -83,13 +77,7 @@ public class Activity_Juego extends AppCompatActivity {
         sensorManager.unregisterListener(gyroscopeEventListener);
     }
 
-    public void startStop() {
-        if(timerRunning){
-            stopTimer();
-        } else {
-            startTimer();
-        }
-    }
+
 
     public void startTimer(){
         countDownTimer = new CountDownTimer(timeLeftinMilliseconds, 1000) {
@@ -105,15 +93,9 @@ public class Activity_Juego extends AppCompatActivity {
             }
         }.start();
 
-        countdownButton.setText("PAUSAR");
         timerRunning = true;
     }
 
-    public void stopTimer(){
-        countDownTimer.cancel();
-        countdownButton.setText("SEGUIR");
-        timerRunning  = false;
-    }
 
     public void updateTimer () {
         int minutes = (int) timeLeftinMilliseconds/60000;
